703e61eb5fb6c339a80859638f0306fe8dda7157
princewilling <princewillingoo@gmail.com>
2023-04-15 13:30:05 +0100

    basic crud operations

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 from contextlib import asynccontextmanager
 from datetime import datetime
-from fastapi import FastAPI, Request
-from cassandra.cqlengine.management import sync_table, drop_table
+from fastapi import FastAPI, HTTPException
+from cassandra.cqlengine.management import sync_table
 
 from models import Post
 from database import get_session
-    
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    global DB_SEESSION
-    DB_SEESSION = get_session()
-    sync_table(Post)
-    yield
-    
-app = FastAPI(lifespan=lifespan)
-
-@app.get("/")
-def homepage(request: Request):
-    return {"meaasage": "Rugged man FastAPI featuring clergey man ASTRA DB"}
-
-###########################################
 
 sample_posts = [
     {
@@ -33,9 +18,49 @@ sample_posts = [
     }
 ]
 
-@app.get("/posts")
+    
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global DB_SEESSION
+    DB_SEESSION = get_session()
+    sync_table(Post)
+    yield
+    
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def homepage():
+    return {"meaasage": "Rugged man FastAPI featuring clergey man ASTRA DB"}
+
+@app.post("/posts")
 def create_post():
     for post in sample_posts:
         post_data = Post.create(**post)
         
     return "Posts created successfully!"
+
+@app.get("/posts")
+def get_posts():
+    posts = [dict(x) for x in Post.objects.all()]
+    return posts
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: str, title: str, body: str):
+    try:
+        post = Post.objects(post_id=post_id).get()
+        post.title = title
+        post.body = body
+        post.save()
+        return "Post updated successfully!"
+    except:
+        raise HTTPException(status_code=404, detail="Post not found")
+    
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: str):
+    try:
+        post = Post.objects(post_id=post_id).get()
+        post.delete()
+        return "Post deleted successfully!"
+    except:
+        raise HTTPException(status_code=404, detail="Post not found")
